7a1c73854613274119c756becc030dd5c937667c
Neradoc <neraOnGit@ri1.fr>
2022-07-29 19:26:10 +0200

    nuff said

--- workflow-directory.js
+++ workflow-directory.js
@@ -8,187 +8,187 @@ let files = document.getElementById("files");
 var current_path = common.current_path;
 
 async function refresh_list() {
-    if (current_path == "") {
-        current_path = "/";
-    }
-    const response = await fetch(new URL("/fs" + current_path, common.workflow_url_base),
-        {
-            headers: {
-                "Accept": "application/json"
-            },
-            credentials: "include"
-        }
-    );
-    const data = await response.json();
-    var new_children = [];
-    var template = document.querySelector('#row');
+	if (current_path == "") {
+		current_path = "/";
+	}
+	const response = await fetch(new URL("/fs" + current_path, common.workflow_url_base),
+		{
+			headers: {
+				"Accept": "application/json"
+			},
+			credentials: "include"
+		}
+	);
+	const data = await response.json();
+	var new_children = [];
+	var template = document.querySelector('#row');
 
-    if (current_path != "/") {
-        var clone = template.content.cloneNode(true);
-        var td = clone.querySelectorAll("td");
-        td[0].innerHTML = "&#128190;";
-        var path = clone.querySelector("a");
-        let parent = new URL("..", "file://" + current_path);
-        path.href = `?path=${parent.pathname}` + window.location.hash;
-        path.innerHTML = "..";
-        // Remove the delete button
-        td[4].replaceChildren();
-        new_children.push(clone);
-    }
+	if (current_path != "/") {
+		var clone = template.content.cloneNode(true);
+		var td = clone.querySelectorAll("td");
+		td[0].innerHTML = "&#128190;";
+		var path = clone.querySelector("a");
+		let parent = new URL("..", "file://" + current_path);
+		path.href = `?path=${parent.pathname}` + window.location.hash;
+		path.innerHTML = "..";
+		// Remove the delete button
+		td[4].replaceChildren();
+		new_children.push(clone);
+	}
 
-    var pwd = document.querySelector('#pwd');
-    var pwd_link = `<a href="?path=/${window.location.hash}" data-path="/">CIRCUITPY</a>/`
-    var fullpath = "/";
-    for(var path of current_path.split("/")) {
-        if(path != "") {
-            fullpath += path + "/";
-            pwd_link += `<a href="?path=${fullpath}${window.location.hash}" data-path="${fullpath}" class="dir">${path}</a>/`;
-        }
-    }
-    pwd.innerHTML = pwd_link;
+	var pwd = document.querySelector('#pwd');
+	var pwd_link = `<a href="?path=/${window.location.hash}" data-path="/">CIRCUITPY</a>/`
+	var fullpath = "/";
+	for(var path of current_path.split("/")) {
+		if(path != "") {
+			fullpath += path + "/";
+			pwd_link += `<a href="?path=${fullpath}${window.location.hash}" data-path="${fullpath}" class="dir">${path}</a>/`;
+		}
+	}
+	pwd.innerHTML = pwd_link;
 
-    data.sort((a,b) => {
-        return a.name.localeCompare(b.name);
-    })
+	data.sort((a,b) => {
+		return a.name.localeCompare(b.name);
+	})
 
-    for (const f of data) {
-        // Clone the new row and insert it into the table
-        var clone = template.content.cloneNode(true);
-        var td = clone.querySelectorAll("td");
-        var file_path = current_path + f.name;
-        let api_url = new URL("/fs" + file_path, common.workflow_url_base);
-        if (f.directory) {
-            file_path = `?path=${file_path}/`;
-            api_url += "/";
-        } else {
-            file_path = api_url;
-        }
-        var icon = "&#10067;";
-        if (current_path == "/" && SECRETS.includes(f.name)) {
-            icon = "🔐";
-        } else if (HIDDEN.includes(f.name)) {
-            continue;
-        } else if (f.name.startsWith(".")) {
-            icon = "🚫";
-        } else if (current_path == "/" && f.name == "lib") {
-            icon = "📚";
-        } else if (f.directory) {
-            icon = "📁";
-        } else if(f.name.endsWith(".txt") ||
-                  f.name.endsWith(".py") ||
-                  f.name.endsWith(".js") ||
-                  f.name.endsWith(".json")) {
-            icon = "📄";
-        } else if (f.name.endsWith(".html")) {
-            icon = "🌐";
-        } else if (f.name.endsWith(".mpy")) {
-            icon = "🐍"; // <img src='blinka.png'/>
-        }
-        td[0].innerHTML = icon;
-        td[1].innerHTML = f.file_size;
-        var path = clone.querySelector("a");
-        path.href = file_path + window.location.hash;
-        path.innerHTML = f.name;
-        td[3].innerHTML = (new Date(f.modified_ns / 1000000)).toLocaleString();
-        var delete_button = clone.querySelector("button.delete");
-        delete_button.value = api_url;
-        delete_button.onclick = del;
+	for (const f of data) {
+		// Clone the new row and insert it into the table
+		var clone = template.content.cloneNode(true);
+		var td = clone.querySelectorAll("td");
+		var file_path = current_path + f.name;
+		let api_url = new URL("/fs" + file_path, common.workflow_url_base);
+		if (f.directory) {
+			file_path = `?path=${file_path}/`;
+			api_url += "/";
+		} else {
+			file_path = api_url;
+		}
+		var icon = "&#10067;";
+		if (current_path == "/" && SECRETS.includes(f.name)) {
+			icon = "🔐";
+		} else if (HIDDEN.includes(f.name)) {
+			continue;
+		} else if (f.name.startsWith(".")) {
+			icon = "🚫";
+		} else if (current_path == "/" && f.name == "lib") {
+			icon = "📚";
+		} else if (f.directory) {
+			icon = "📁";
+		} else if(f.name.endsWith(".txt") ||
+				  f.name.endsWith(".py") ||
+				  f.name.endsWith(".js") ||
+				  f.name.endsWith(".json")) {
+			icon = "📄";
+		} else if (f.name.endsWith(".html")) {
+			icon = "🌐";
+		} else if (f.name.endsWith(".mpy")) {
+			icon = "🐍"; // <img src='blinka.png'/>
+		}
+		td[0].innerHTML = icon;
+		td[1].innerHTML = f.file_size;
+		var path = clone.querySelector("a");
+		path.href = file_path + window.location.hash;
+		path.innerHTML = f.name;
+		td[3].innerHTML = (new Date(f.modified_ns / 1000000)).toLocaleString();
+		var delete_button = clone.querySelector("button.delete");
+		delete_button.value = api_url;
+		delete_button.onclick = del;
 
-        new_children.push(clone);
-    }
-    var tbody = document.querySelector("tbody");
-    tbody.replaceChildren(...new_children);
-    var loading = document.querySelector('#file_list_loading_image');
-    loading.style.display = "none";
+		new_children.push(clone);
+	}
+	var tbody = document.querySelector("tbody");
+	tbody.replaceChildren(...new_children);
+	var loading = document.querySelector('#file_list_loading_image');
+	loading.style.display = "none";
 }
 
 async function find_devices() {
-    var response = await fetch(new URL("/cp/devices.json", common.workflow_url_base));
-    const data = await response.json();
-    refresh_list();
+	var response = await fetch(new URL("/cp/devices.json", common.workflow_url_base));
+	const data = await response.json();
+	refresh_list();
 }
 
 async function mkdir(e) {
-    const response = await fetch(
-        new URL("/fs" + current_path + new_directory_name.value + "/", common.workflow_url_base),
-        {
-            method: "PUT",
-            headers: {
-                'X-Timestamp': Date.now()
-            }
-        }
-    );
-    if (response.ok) {
-        refresh_list();
-        new_directory_name.value = "";
-        mkdir_button.disabled = true;
-    }
+	const response = await fetch(
+		new URL("/fs" + current_path + new_directory_name.value + "/", common.workflow_url_base),
+		{
+			method: "PUT",
+			headers: {
+				'X-Timestamp': Date.now()
+			}
+		}
+	);
+	if (response.ok) {
+		refresh_list();
+		new_directory_name.value = "";
+		mkdir_button.disabled = true;
+	}
 }
 
 async function upload(e) {
-    console.log("upload");
-    for (const file of files.files) {
-        console.log(file);
-        let file_path = new URL("/fs" + current_path + file.name, common.workflow_url_base);
-        console.log(file_path);
-        const response = await fetch(file_path,
-            {
-                method: "PUT",
-                headers: {
-                    'Content-Type': 'application/octet-stream',
-                    'X-Timestamp': file.lastModified
-                },
-                body: file
-            }
-        )
-        if (response.ok) {
-            refresh_list();
-            console.log(files);
-            files.value = "";
-            upload_button.disabled = true;
-        }
-    }
+	console.log("upload");
+	for (const file of files.files) {
+		console.log(file);
+		let file_path = new URL("/fs" + current_path + file.name, common.workflow_url_base);
+		console.log(file_path);
+		const response = await fetch(file_path,
+			{
+				method: "PUT",
+				headers: {
+					'Content-Type': 'application/octet-stream',
+					'X-Timestamp': file.lastModified
+				},
+				body: file
+			}
+		)
+		if (response.ok) {
+			refresh_list();
+			console.log(files);
+			files.value = "";
+			upload_button.disabled = true;
+		}
+	}
 }
 
 async function del(e) {
-    console.log("delete", e.target.value);
-    console.log(e);
-    let fn = new URL(e.target.value);
-    var prompt = "Delete " + fn.pathname.substr(3);
-    if (e.target.value.endsWith("/")) {
-        prompt += " and all of its contents?";
-    } else {
-        prompt += "?";
-    }
-    if (confirm(prompt)) {
-        console.log(e.target.value);
-        const response = await fetch(e.target.value,
-            {
-                method: "DELETE",
-                headers: common.headers(),
-            }
-        )
-        if (response.ok) {
-            refresh_list();
-        } else {
-            const message = `Deleting ${fn.pathname.substr(3)} failed`
-            const status = await response.status;
-            const error = await response.statusText;
-            switch(status) {
-            case 401:
-                console.log(`${message}: Bad password !`);
-                break;
-            case 403:
-                console.log(`${message}: Not authorized !`);
-                break;
-            case 409:
-                console.log(`${message}: Drive read-only !`);
-                break;
-            default:
-                console.log(`${message}: ${error} !`);
-            }
-        }
-    }
+	console.log("delete", e.target.value);
+	console.log(e);
+	let fn = new URL(e.target.value);
+	var prompt = "Delete " + fn.pathname.substr(3);
+	if (e.target.value.endsWith("/")) {
+		prompt += " and all of its contents?";
+	} else {
+		prompt += "?";
+	}
+	if (confirm(prompt)) {
+		console.log(e.target.value);
+		const response = await fetch(e.target.value,
+			{
+				method: "DELETE",
+				headers: common.headers(),
+			}
+		)
+		if (response.ok) {
+			refresh_list();
+		} else {
+			const message = `Deleting ${fn.pathname.substr(3)} failed`
+			const status = await response.status;
+			const error = await response.statusText;
+			switch(status) {
+			case 401:
+				console.log(`${message}: Bad password !`);
+				break;
+			case 403:
+				console.log(`${message}: Not authorized !`);
+				break;
+			case 409:
+				console.log(`${message}: Drive read-only !`);
+				break;
+			default:
+				console.log(`${message}: ${error} !`);
+			}
+		}
+	}
 }
 
 async function setup_directory() {
