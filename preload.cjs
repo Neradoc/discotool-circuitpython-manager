6692a3f624e0a66147aaae07f83e02e67a1796af
Neradoc <neraOnGit@ri1.fr>
2022-08-25 14:07:27 +0200

    add shell module

--- preload.cjs
+++ preload.cjs
@@ -1,15 +1,18 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 
-const log = require('electron-log');
-window.log = log.functions;
+const log = require('electron-log')
+window.log = log.functions
 
-window.modulePath = require("path");
-window.moduleDrivelist = require("drivelist");
-window.moduleFss = require("fs"); // sync
-window.moduleFs = require("fs/promises");
-window.moduleFsx = require("fs-extra");
-window.moduleMdns = require('multicast-dns')()
+const { shell } = require('electron')
+
+window.modulePath = require("path")
+window.moduleDrivelist = require("drivelist")
+window.moduleFss = require("fs") // sync
+window.moduleFs = require("fs/promises")
+window.moduleFsx = require("fs-extra")
+window.moduleMdns = require('multicast-dns')
+window.shell = shell
 
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
