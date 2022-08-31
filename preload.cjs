0689e16e528bfc47f19e3b5bfc33a41472d64a41
Neradoc <neraOnGit@ri1.fr>
2022-08-31 13:46:29 +0200

    remove electron-log

--- preload.cjs
+++ preload.cjs
@@ -1,9 +1,6 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 
-const log = require('electron-log')
-window.log = log.functions
-
 const { shell, ipcRenderer } = require('electron')
 
 window.modulePath = require("path")
