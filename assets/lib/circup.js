1f540bb070e9c09eb0160037d07c3d1f08fc3137
Neradoc <neraOnGit@ri1.fr>
2022-09-03 10:43:19 +0200

    fix reading and updating modules with subdirectories

--- assets/lib/circup.js
+++ assets/lib/circup.js
@@ -35,16 +35,41 @@ class Circup {
 			console.log("Installing", module_name);
 			var module = this.library_bundle.get_module(module_name);
 			var module_files = await this.library_bundle.list_module_files(module);
+			var module_base = `/lib/${module.name}/`
 			if(module.package) {
-				await this.workflow.create_dir(`/lib/${module.name}/`);
+				await this.workflow.create_dir(module_base);
 			}
 			for(var file of module_files) {
-				var upload_path = file.name.replace(/^[^\/]+\//, "/");
+				const upload_path = file.name.replace(/^[^\/]+\//, "/");
+				// list and create the intermediary sub-directories if needed
+				const parts = upload_path.split("/").filter((x) => x.length)
+				const subdirs = parts.slice(2,-1)
+				var dir_path = module_base
+				for(const sub of subdirs) {
+					dir_path = `${dir_path}${sub}/`
+					await this.workflow.create_dir(dir_path);
+				}
 				// var upload_path = file.name.replace(/^.+?\/lib\//, "/lib/");
 				const file_content = await file.async("blob");
 				await this.workflow.upload_file(upload_path, file_content);
 			}
 		}
+	}
+
+	async sublist(cur_dir, module_files) {
+		const response = await this.workflow.list_dir(cur_dir + "/");
+		const these_files = response.content;
+		for(var ffile of these_files) {
+			const full_path = `${cur_dir}/${ffile.name}`
+			if(ffile.directory) {
+				await this.sublist(full_path, module_files)
+			} else {
+				if(ffile.name.endsWith(".py") || ffile.name.endsWith(".mpy")) {
+					module_files.push(full_path)
+				}
+			}
+		}
+		module_files = module_files.map((item) => cur_dir + "/" + item.name);
 	}
 
 	async get_module_version(module_name, board_libs=null) {
@@ -58,10 +83,8 @@ class Circup {
 		var cpver = await this.workflow.cp_version();
 
 		if(pkg && board_libs.includes(module.name)) {
-			// look at all files in the package
-			var response = await this.workflow.list_dir(module_path + "/");
-			module_files = response.content;
-			module_files = module_files.map((item) => module_path + "/" + item.name);
+			// look at all files in the package recursively
+			await this.sublist(module_path, module_files)
 		} else if(!pkg && board_libs.includes(module.name + ".py")) {
 			module_files = [module_path+".py"];
 		} else if(!pkg && board_libs.includes(module.name + ".mpy")) {
@@ -72,7 +95,8 @@ class Circup {
 
 		var version = false;
 		for(var file_name of module_files) {
-			var response = await this.workflow.get_file_content("/" + file_name);
+			var file_path = ("/" + file_name).replace(/\/\/+/, "/")
+			var response = await this.workflow.get_file_content(file_path);
 			if(!response.ok) { continue }
 			var file_data = response.content;
 			// empty MPY files are bad
