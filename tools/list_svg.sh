8a58670eb0e5cd4192239bff4660f4727a966d8d
Neradoc <neraOnGit@ri1.fr>
2022-09-10 14:45:13 +0200

    filter some directories in forge, add tools

---
+++ tools/list_svg.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+function greppit() {
+	git grep --extended-regexp "([a-z._-]+\.svg)" | perl -ne 'm/([a-z._-]+\.svg)/; print("$1\n")'
+	git grep --extended-regexp "_icon\s*\(\s*[\"'][a-z._-]+[\"']\s*\)" | perl -ne 'm/_icon\s*\(\s*["'"'"']([a-z._-]+)["'"'"']\s*\)/; print("$1.svg\n")'
+}
+
+list=`greppit | sort | uniq`
+
+function full_list() {
+	for file in $list; do
+		echo assets/svg/$file
+		echo assets/svg/$file.license
+	done
+}
+
+flist=`full_list`
+
+function unused_files() {
+	for file in assets/svg/*; do
+		if [[ "$flist" == *"$file"* ]]; then
+			# nope
+			no="no"
+		else
+			echo $file
+		fi
+	done
+}
+
+# Unused SVG files
+unused_files
---
+++ tools/run.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+if [[ "$1" == build ]]
+then
+	npm run make
+	# electron-packager . CirpyManager --overwrite --platform win32 --arch x64 --out _build/ --ignore="^/_.*"
+	# electron-packager . CirpyManager --overwrite --platform darwin --out _build/ --ignore="^/_.*"
+
+	# electron-packager . CirpyManager --overwrite --asar --platform=win32 --arch=ia32 --icon=assets/icons/win/icon.ico --prune=true --out=_build --version-string.CompanyName=Neradoc --version-string.FileDescription=Neradoc --version-string.ProductName="Cirpy Manager"
+
+	# electron-packager . CirpyManager --all --out _build/
+	# https://www.npmjs.com/package/electron-installer-windows
+	
+	# ./node_modules/.bin/electron-rebuild
+else
+	npm start
+	# npx electron .
+fi
+
+if [[ "$1" == "SCROUBOULIFLIPELCHABIDOU" ]]
+then
+
+npm i -D electron
+npm i -D electron-rebuild
+./node_modules/.bin/electron-rebuild
+npm i drivelist
+./node_modules/.bin/electron-rebuild
+npm i fs-extra
+./node_modules/.bin/electron-rebuild
+npm i multicast-dns
+./node_modules/.bin/electron-rebuild
+npm i node-powershell
+./node_modules/.bin/electron-rebuild
+npm i path
+./node_modules/.bin/electron-rebuild
+
+npm install --save-dev @electron-forge/cli
+npx electron-forge import
+
+npm run make
+
+fi
